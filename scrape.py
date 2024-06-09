b35d5e82b46e30fb113d62fb18343829fb6a2c88
Keegan <keeganpatton@gmail.com>
2024-06-08 21:02:10 -0500

    broke up program into modules

---
+++ scrape.py
@@ -0,0 +1,82 @@
+from selenium.common.exceptions import StaleElementReferenceException, NoSuchElementException, TimeoutException
+from selenium.webdriver.common.action_chains import ActionChains
+from site_utils import scroll_page, scrape_data # UDF Imports
+from selenium import webdriver
+import pandas as pd
+
+
+#####################
+#  Core Variables   #
+#####################
+
+thrasher_site = 'https://www.thrashermagazine.com/articles-and-interviews/'
+driver = webdriver.Firefox()
+driver.get(thrasher_site)
+actions = ActionChains(driver)
+
+
+# Resusable Vars
+scroll_page_cmd = "window.scrollTo(0, document.body.scrollHeight);"
+page_height = "return document.body.scrollHeight"
+item_target_count = 10
+counter = 0
+
+
+# The script will loop through articles...
+# Creating dicts of questions and answers...
+# Then append each dict in this list...
+articles_data = []
+
+
+# This keeps track of what we have seen in the loop...
+# After each click that link will be added to this set..
+clicked_links = set()
+
+
+
+#####################
+#   Extract Logic   #
+#####################
+
+# Limit this by the target count, otherwise it will run infinitely...
+# Each Iteration of this loop will increment to that point...
+while counter < item_target_count: 
+    
+    elements = scroll_page(driver=driver, page_height=page_height, scroll_page_cmd=scroll_page_cmd)    
+
+    try:
+        for element in elements:
+            try:
+
+                # Look for each clickable title link
+                link = element.get_attribute('href')
+                
+                # clicked_links is to help with redundancy between iterations...
+                # This was not a problem, just for incase the redirect does something weird...
+                if link not in clicked_links:
+                    clicked_links.add(link)
+                    driver.get(f'{link}')
+                    
+                    scrape_data(link=f'{link}', lst=articles_data)
+                    driver.back()    
+                    counter+=1    
+                    print(counter)
+                    
+                    if counter >= item_target_count:
+                        break
+
+            # Not really sure why I need two try excepts...
+            # But it works and I was running into insurmountable stale elements...
+            except (StaleElementReferenceException, NoSuchElementException):
+                    continue  
+
+    except TimeoutException:
+            continue
+
+
+
+#####################
+# Data Prep for ORM #
+#####################
+df = pd.DataFrame(articles_data)
+df.to_csv('./data/data.csv')
---
+++ site_utils.py
@@ -0,0 +1,90 @@
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup 
+import requests
+import time
+import re
+
+
+
+
+def get_web_elements(driver):
+    """Locate all instances of <a> tags indicating an article title.
+    :Inputs: a selenium Web Driver
+    """ 
+    
+    return WebDriverWait(driver, 10).until(
+            EC.presence_of_all_elements_located(
+                (By.CSS_SELECTOR, 'a.post-title-link')
+        )
+    )
+
+
+
+
+def scroll_page(driver, page_height,  scroll_page_cmd):
+    """Navigate an infinite scroll page and retrieve html elements
+    incrementally.
+
+    :param driver: A Selenium Web Driver
+    :param page_height: JS Selenium input as {return document.body.scrollHeight}
+    :param scroll_page_cmd:JS Selenium input as {window.scrollTo(0, document.body.scrollHeight);}
+    """
+
+    driver.execute_script(scroll_page_cmd)
+    time.sleep(5)
+
+    last_height = page_height
+    new_height = driver.execute_script(page_height)
+    
+    if new_height == last_height:
+        return None 
+    last_height = new_height
+
+    return get_web_elements(driver)
+
+
+
+
+def scrape_data(link, lst):
+    """Iterates through a webpage, looking for <strong> and <em> tags,
+    then finds the next <br> tag, to locate the text sibling. A check
+    is performed to ensure that text is indeed found as a {str}.
+    Creates an array of dicts.
+
+    :param link: Web Page URL.
+    :param lst: Provide an empty array to append results to.
+    """
+
+    http_req = requests.get(f'{link}')
+    soup = BeautifulSoup(http_req.content, 'html.parser')
+    title = soup.find('h3') 
+    questions = soup.find_all(['strong', 'em'])
+    
+    for question in questions:
+
+        try:
+            # This is to strip the tags for questions...
+            # Because I could not figure out how to do it otherwise...
+            remove_question_tags = re.compile('<.*?>')
+            question_text = re.sub(remove_question_tags, "", question.text) 
+            
+            
+            next_br = question.find_next('br')
+            answer = next_br.next_sibling
+
+            # Make sure that the following sibling is actually text...
+            # Then create dicts stored in our list for later...
+            if isinstance(answer, str):
+                
+                lst.append({
+                    "title" : title,
+                    "question" : question_text,
+                    "answer" : answer
+                })
+        
+        except Exception as e:
+            print(f"error is {e}")
+            continue
+
